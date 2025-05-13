55b113eb3cc32ebe738a79c9aadcbbafe6932d8b
romanpidvirnyi <roman.pidvirnyi@gmail.com>
2025-05-13 12:07:51 +0300

    fix url generating

--- backend/proshield/main.py
+++ backend/proshield/main.py
@@ -55,12 +55,3 @@ app.mount("/static", StaticFiles(directory=STATIC_PATH), name="static")
 # Include routers
 app.include_router(api_router, prefix="/api", tags=["API"])
 app.include_router(views_routes)
-
-
-if __name__ == "__main__":
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=8000,
-        reload=True,
-    )
